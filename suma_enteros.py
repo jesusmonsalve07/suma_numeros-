7744c34382d31480962375a07f4bcef068dacacd
jesusmonsalve07 <jesus2224505@correo.uis.edu.co>
2022-05-03 15:23:42 -0500

    Desarrollo: suma de dos enteros igual a tercero

---
+++ suma_enteros.py
@@ -0,0 +1,26 @@
+"Programa para que la suma de los dos primeros numeros sea igual al tercer numero"
+
+print("""-----------------------------------------""")
+print("""-------- suma de dos numeros-------------""")
+print("""-----------------------------------------""")
+
+#input
+
+x = int(input("Digite el primer numero: "))
+y = int(input("Digite el segundo numero: "))
+z = int(input("Digite el tercer numero: "))
+
+
+#processsing 
+
+if z == x + y :
+   msj =  print(" cumple") 
+
+else : 
+   msj = print(" no cumple")
+
+
+# output
+
+
+print("El numero digitado ",  msj)
